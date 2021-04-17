fcda389feb2b8f3f3137c728422bea31a9cffc82
brahim ezz <ezzdinib@gmail.com>
2021-04-17 14:00:47 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcom to  <code>Be/Coding </code> 
+        </p>
+        <a
+          className="App-link"
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Brahim Ezzdini
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
